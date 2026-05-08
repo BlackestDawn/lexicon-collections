35dd6282b2c7886be2738cdc601d18556a1933f6
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-08 14:35:49 +0200

    added unit test for adding vehicle

---
+++ Ovn4_CollectionsTesting/GarageTests.cs
@@ -0,0 +1,21 @@
+﻿using Ovn4_Collections.Models;
+using Ovn4_Collections.Models.Vehicles;
+using Ovn4_Collections.Services;
+
+namespace Ovn4_CollectionsTesting;
+
+public class GarageTests
+{
+    [Fact]
+    public void AddingVehicle()
+    {
+        Vehicle vehicle = new("ADB123", new FuelEnginesStats(150, 1.7m, FuelTypes.Gasoline), 4, "red");
+        Vehicle[] expected = [ vehicle ];
+        Garage garage = new(3);
+
+        garage.AddVehicle(vehicle);
+        var result = garage.GetAllVehicles();
+
+        Assert.Equal(expected, result);
+    }
+}
