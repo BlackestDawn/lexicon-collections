08c1c3392ff58966f261792fb3075d2bf94c5462
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-08 14:45:11 +0200

    added unit test for bulk adding vehicles

--- Ovn4_CollectionsTesting/GarageTests.cs
+++ Ovn4_CollectionsTesting/GarageTests.cs
@@ -18,4 +18,20 @@ public class GarageTests
 
         Assert.Equal(expected, result);
     }
+
+    [Fact]
+    public void BulkAddingVehicles()
+    {
+        Vehicle[] vehicles = [
+            new("ABC123", new FuelEnginesStats(150, 2.0m, FuelTypes.Gasoline), 4, "Red"),
+            new("XYZ789", new FuelEnginesStats(320, 3.0m, FuelTypes.Diesel), 4, "Black"),
+            new("EV0001", new ElectricEnginesStats(408, 100.0m), 4, "White"),
+        ];
+        Garage garage = new(5);
+
+        garage.BulkLoadVehicles(vehicles);
+        var result = garage.GetAllVehicles();
+
+        Assert.Equal(vehicles, result);
+    }
 }
