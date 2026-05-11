bde7eb2734d69886230ebece135927aaf955cd9c
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-11 11:38:59 +0200

    added Boat sub-class

---
+++ Ovn4_Collections/Models/Vehicles/Boat.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using Ovn4_Collections.Extensions;
+
+namespace Ovn4_Collections.Models.Vehicles;
+
+public class Boat : Vehicle
+{
+    private int _engineCount;
+
+    public Boat(VehicleTypes vehicleType, string licenceNumber, int engineCount, IEngine engine, int numWheels, string color)
+        : base(vehicleType, licenceNumber, engine, numWheels, color)
+    {
+        this._engineCount = engineCount;
+    }
+
+    public override string FullDescription()
+    {
+        StringBuilder sb = new StringBuilder(base.FullDescription());
+
+        sb.InsertLine(1, $"Engine count: {this._engineCount}");
+
+        return sb.ToString();
+    }
+}
