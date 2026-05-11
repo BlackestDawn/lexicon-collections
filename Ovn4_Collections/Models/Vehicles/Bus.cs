4c081f5574dade5fbc1e3359051ecea6e9ab7d16
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-11 11:30:32 +0200

    added Bus sub-class

---
+++ Ovn4_Collections/Models/Vehicles/Bus.cs
@@ -0,0 +1,25 @@
+using System.Text;
+using Ovn4_Collections.Extensions;
+
+namespace Ovn4_Collections.Models.Vehicles;
+
+public class Bus : Vehicle
+{
+    private int _passengerCapacity;
+
+    public Bus(VehicleTypes vehicleType, string licenceNumber, int passangerCapacity, IEngine engine, int numWheels, string color)
+        : base(vehicleType, licenceNumber, engine, numWheels, color)
+    {
+        this._passengerCapacity = passangerCapacity;
+    }
+
+    public override string FullDescription()
+    {
+        StringBuilder sb = new StringBuilder(base.FullDescription());
+
+        sb.InsertLine(1, $"Passenger capacity: {this._passengerCapacity}");
+        sb.AppendLine("Var version");
+
+        return sb.ToString();
+    }
+}
