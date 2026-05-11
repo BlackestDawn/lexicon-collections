b6015d245a5cb2c91ff1d2376b4bc9b9810201e8
Alexander Stauch <blacke4dawn@gmail.com>
2026-05-11 09:47:59 +0200

    added two description methods to Vehicle class

--- Ovn4_Collections/Models/Vehicles/Vehicle.cs
+++ Ovn4_Collections/Models/Vehicles/Vehicle.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Ovn4_Collections.Models.Vehicles;
 
 public class Vehicle(VehicleTypes vehicleType, string licenceNumber, IEngine engine, int numWheels, string color)
@@ -28,4 +30,21 @@ public class Vehicle(VehicleTypes vehicleType, string licenceNumber, IEngine eng
         }
 
     public override int GetHashCode() => LicenceNumber.GetHashCode();
+
+    public string MinimalDescription()
+    {
+        return $"Licence: {this._licenceNumber}, Type: {this._vehicleType}";
+    }
+
+    public virtual string FullDescription()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        string wheelsDescriptionPart = this._numWheels > 0 ? $", Wheels: {this._numWheels}" : "";
+        sb.AppendLine(this.MinimalDescription());
+        sb.AppendLine($"Color: {this._color}{wheelsDescriptionPart}");
+        sb.AppendLine($"Engine: {this._engine.Description}");
+
+        return sb.ToString();
+    }
 }
